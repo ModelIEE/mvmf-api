04bec95a2a396a253ece10294201c2934c3aa6fd
Felix Rinker <felix.rinker@gmail.com>
2022-04-02 07:10:34 +0200

    Adapt generation API to new generate interface

--- mvmf-generation-api/src/main/java/modeliee/mvmf/api/generation/IGenerator.java
+++ mvmf-generation-api/src/main/java/modeliee/mvmf/api/generation/IGenerator.java
@@ -6,23 +6,20 @@ package modeliee.mvmf.api.generation;
 /**
  * Generator interface.
  *
- * @param <T> the source model type
- * @param <K> the target model type
+ * @param <C> the input generator config
+ * @param <K> the input base model
+ * @param <T> the target model to generate
  */
-public interface IGenerator<T, K, C> {
+public interface IGenerator<C, T, K> {
 
     /**
      * Generates the source object model into the target model
      *
-     * @param sourceModel the input object graph
-     * @param targetModel the output stream
+     * @param generatorConfig the input generator config of type {@link T}
+     * @param baseModel       the input base model of type {@link K}
+     * @return the generated model of type {@link K}
      * @throws GenerationException if an error occurs
      */
-    void transform(T sourceModel, K targetModel) throws GenerationException;
+    T generate(C generatorConfig, K baseModel) throws GenerationException;
 
-    /**
-     *
-     * @param config the configuration for the transformer
-     */
-    void setConfig(C config);
 }
